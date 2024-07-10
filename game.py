4fb8e3f0d6ab7e89cd9d15a3ca88a76abb43f6fc
jong2.ha <jong2.ha@samsung.com>
2024-07-10 14:09:36 +0900

    [Feature] 39

--- game.py
+++ game.py
@@ -2,9 +2,13 @@ class Game:
     def __init__(self):
         pass
 
-    def guess(self, guessNumber):
+    def guess(self, guessNumber:str):
         if guessNumber is None:
             raise TypeError
 
         if len(guessNumber) != 3:
             raise TypeError
+
+        for ch in guessNumber:
+            if not ch.isdigit():
+                raise TypeError
--- test_game.py
+++ test_game.py
@@ -13,7 +13,9 @@ class TestGame(TestCase):
             self.fail()
         except TypeError:
             pass
+
     def test_exception_when_invalid_input(self):
         self.assert_illegal_argument(None)
         self.assert_illegal_argument('12')
         self.assert_illegal_argument('1234')
+        self.assert_illegal_argument('12a')
