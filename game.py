5a0c8307e2b470f70915268fd5892a072d70bea2
jong2.ha <jong2.ha@samsung.com>
2024-07-10 14:36:23 +0900

    [Feature] 47

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
+from game_result import GameResult
+
+
 class Game:
     def __init__(self):
-        pass
+        self.question = ""
 
     def guess(self, guessNumber: str):
         self.assert_illegal_value(guessNumber)
+
+        return GameResult(True, 3, 0)
 
     def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
